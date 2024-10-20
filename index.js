9139d947d65a1d15677f1e637952474461851dc8
Xayritdinov <rafa.chgm@gmail.com>
2024-10-21 00:23:46 +0500

    DZ №19

--- index.js
+++ index.js
@@ -1,23 +1,50 @@
-const API_KEY = "85fc768d"
+const API_KEY = "85fc768d";
 
 async function fetchData(title) {
-    const response = await fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&t=${title}`)
-    const data = await response.json()
-    return data
+  const response = await fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&t=${title}`);
+  const data = await response.json();
+  return data;
 }
 
-const searchInputElement = document.querySelector('#movie-search-input')
-const searchButtonElement= document.querySelector('#movie-search-button')
+const searchInputElement = document.querySelector('#movie-search-input');
+const searchButtonElement = document.querySelector('#movie-search-button');
 
 const modalTitleElement = document.querySelector("#exampleModalLabel");
 const modalBodyElement = document.querySelector(".modal-body");
 
-let movieTitleValue = ''
+const searchResultsContainer = document.querySelector('.search-results');
+const spinnerElement = document.querySelector('#loading-spinner');
+const toastSuccess = document.querySelector('#toast-success');
+const toastFail = document.querySelector('#toast-fail');
+
+let movieTitleValue = '';
+
+function showToast(toastElement) {
+  const toast = new bootstrap.Toast(toastElement);
+  toastElement.style.display = 'block';
+  toast.show();
+  setTimeout(() => {
+    toastElement.style.display = 'none';
+  }, 3000);
+}
 
 searchButtonElement.addEventListener('click', async () => {
-    movieTitleValue = searchInputElement.value
-    const movie = await fetchData(movieTitleValue)
-    const cardElementTemplate = `
+  movieTitleValue = searchInputElement.value;
+
+  while (searchResultsContainer.firstChild) {
+    searchResultsContainer.removeChild(searchResultsContainer.firstChild);
+  }
+
+  spinnerElement.style.display = 'block';
+
+  setTimeout(async () => {
+    const movie = await fetchData(movieTitleValue);
+    spinnerElement.style.display = 'none';
+
+    searchInputElement.value = '';
+
+    if (movie.Response === "True") {
+      const cardElement = `
         <div class="card" style="width: 18rem">
             <img
             src="${movie.Poster}"
@@ -38,43 +65,36 @@ searchButtonElement.addEventListener('click', async () => {
                 </a>
             </div>
         </div>
-    `
-    
-    const searchResultsContainer = document.querySelector('.search-results')
-        
-    if (searchResultsContainer.firstElementChild) {        
-        searchResultsContainer.removeChild(searchResultsContainer.firstElementChild)
-    }
-    
-    searchResultsContainer.insertAdjacentHTML('beforeend', cardElementTemplate)
-    searchInputElement.value = ''
+    `      
+      searchResultsContainer.insertAdjacentHTML('beforeend', cardElement)
 
-    const detailsButton = document.querySelector("#details-button");
-    
-    detailsButton.addEventListener("click", () => {
-        modalTitleElement.textContent = movie.Title
+      showToast(toastSuccess);
+      
+      const detailsButton = document.querySelector("#details-button");
+      detailsButton.addEventListener("click", () => {
+        modalTitleElement.textContent = movie.Title;
         while (modalBodyElement.firstChild) {
-            modalBodyElement.removeChild(modalBodyElement.firstChild);
+          modalBodyElement.removeChild(modalBodyElement.firstChild);
         }
-
         const modalContent = `
-            <div class="d-flex">
-                <div class="me-3">
-                    <img src="${movie.Poster}" alt="${movie.Title} poster" class="img-fluid" style="max-width: 150px; height: auto;">
-                </div>
-                <div>
-                    <p><strong>Год выпуска:</strong> ${movie.Year}</p>
-                    <p><strong>Режиссёр:</strong> ${movie.Director}</p>
-                    <p><strong>Актёры:</strong> ${movie.Actors}</p>
-                    <p><strong>Жанр:</strong> ${movie.Genre}</p>
-                    <p><strong>Рейтинг IMDb:</strong> ${movie.imdbRating}</p>
-                    <p><strong>Описание:</strong> ${movie.Plot}</p>
-                </div>
-            </div>
-        `
-
-        modalBodyElement.insertAdjacentHTML('beforeend', modalContent)
-    });
-})
-
-
+          <div class="d-flex">
+              <div class="me-3">
+                  <img src="${movie.Poster}" alt="${movie.Title} poster" class="img-fluid" style="max-width: 200px; height: auto;">
+              </div>
+              <div>
+                  <p><strong>Год выпуска:</strong> ${movie.Year}</p>
+                  <p><strong>Режиссёр:</strong> ${movie.Director}</p>
+                  <p><strong>Актёры:</strong> ${movie.Actors}</p>
+                  <p><strong>Жанр:</strong> ${movie.Genre}</p>
+                  <p><strong>Рейтинг IMDb:</strong> ${movie.imdbRating}</p>
+                  <p><strong>Описание:</strong> ${movie.Plot}</p>
+              </div>
+          </div>
+        `;
+        modalBodyElement.insertAdjacentHTML('beforeend', modalContent);
+      });
+    } else {
+      showToast(toastFail);
+    }
+  }, 1000);
+});
